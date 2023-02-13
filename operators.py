04ffae7ff126146e79621dafbdc369991a0c72b6
Stephen Muchendu <muchendumaina66@gmail.com>
2023-02-13 13:13:57 +0300

    additions

---
+++ operators.py
@@ -0,0 +1,21 @@
+name = 'Steved'
+bool = True
+
+print(type(name) is not type(bool))
+
+print(31 // 4)
+
+x = 5 # binary: 101
+y = 3 # binary: 011
+print(x & y)
+
+myAges = [12, 13, 14, 15]
+
+print(12 in myAges)
+
+alpha = [1, 2, 3]
+beta = alpha # an alias for alpha 
+beta += [4,5] 
+print(beta)
+
+print('My name is Steve')
